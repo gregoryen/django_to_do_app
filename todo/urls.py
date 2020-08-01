e9c2e0a39ec624f19bd58ea473f44721cd958cb6
gregoryen <gregnerga@gmail.com>
2020-08-02 00:18:37 +0200

    add main app view

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,5 @@ app_name = 'todo'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('new_task/', views.new_task, name='new_task')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-from . import models
+from .models import ToDo
 
 # Create your views here.
 
+
 def home(request):
     return render(request, template_name='todo/index.html')
+
+
+def new_task(request):
+
+    # Tworze nowy wpis do bazy danych
+
+    # Pobieram list wpisow z bazy danych i tworze ich liste
+
+    # Stworzona liste zwracam
+
+    return render(request, 'todo/new_task.html')
+
+
+def delete_task(request):
+
+    # Wyszukuje i usuwam dany wpis
+
+    # Pobieram list wpisw z bazy danych i tworze ich liste
+
+    # Zwracam stworzona liste
+
+    return render(request, 'todo/new_task.html')
