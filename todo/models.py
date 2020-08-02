14faebfead9ca2383967d47a163537e224420f9e
gregoryen <gregnerga@gmail.com>
2020-08-02 14:48:44 +0200

    new_task styling

--- todo/models.py
+++ todo/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class ToDo(models.Model):
     added_date = models.DateTimeField()
     text = models.CharField(max_length=200)
+    is_urgent = models.BooleanField()
 
     class Meta:
         verbose_name_plural = "Todo's"
