ce11c6105c2c7d7c76fa0d437bcc4b26a1e807b1
gregoryen <gregnerga@gmail.com>
2020-08-02 21:47:53 +0200

    add delete functionality

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,6 @@ app_name = 'todo'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('new_task/', views.new_task, name='new_task')
+    path('new_task/', views.new_task, name='new_task'),
+    path('delete_task/<int:todo_id>', views.delete_task, name='delete_task')
 ]
